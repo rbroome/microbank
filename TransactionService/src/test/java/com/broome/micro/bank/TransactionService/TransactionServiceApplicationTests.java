f7dc7ce69e9f19079b836dc38c123f9976ee6106
Robin Broome <robin.broome@extendaretail.com>
2019-02-10 12:51:46 +0100

    Adding TransactionService

---
+++ TransactionService/src/test/java/com/broome/micro/bank/TransactionService/TransactionServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.broome.micro.bank.TransactionService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
